211a444f266bf29557c3c937bc71ad03401b4bac
Noman Chowdhury <nomanchow79@gmail.com>
2023-11-24 21:21:06 +0600

    Update MQTTSNbridge.py

--- MQTTSN_Client/MQTTSNbridge.py
+++ MQTTSN_Client/MQTTSNbridge.py
@@ -3,54 +3,54 @@ import MQTTSNclient
 import json
 import boto3
 
-# connection to DynamoDB and access to the table EnvironmentalStationDB
+# Connection to DynamoDB and access to the table EnvironmentalStationDB
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 dynamoTable = dynamodb.Table('EnvironmentalStationDB')
-jsonP = ''
+jsonP = ''  # Variable to store JSON payload
 
-# clients for MQTT and MQTTS
+# Instantiate MQTT and MQTT-SN clients
 MQTTClient = AWSIoTMQTTClient("MQTTSNbridge")
 MQTTSNClient = MQTTSNclient.Client("bridge", port=1885)
 
 class Callback:
-  # function that replies a message from the MQTTSN broker to the MQTT one
-  # and inserts into the database the message just arrived
-  def messageArrived(self, topicName, payload, qos, retained, msgid):
-      message = payload.decode("utf-8")
-      jsonP = json.loads(message) 
-      print(topicName, message)
-      MQTTClient.publish(topicName, message, qos)
-      dynamoTable.put_item(Item=jsonP)
-      return True
+    # Function that handles incoming messages from the MQTT-SN broker
+    # It publishes the message to the MQTT broker and stores it in DynamoDB
+    def messageArrived(self, topicName, payload, qos, retained, msgid):
+        # Decode the payload and load it as JSON
+        message = payload.decode("utf-8")
+        jsonP = json.loads(message)  # Update global JSON payload variable
+        print(topicName, message)  # Print received topic and message
+        MQTTClient.publish(topicName, message, qos)  # Publish the message to MQTT broker
+        dynamoTable.put_item(Item=jsonP)  # Store the message payload in DynamoDB
+        return True
 
-# path that indicates the certificates position
+# Path indicating the certificates position
 path = "../certs/"
 
-# configure the access with the AWS MQTT broker
-MQTTClient.configureEndpoint(apikey, 8883)
-MQTTClient.configureCredentials(path+"root-CA.crt",
-                                path+"private.pem.key",
-                                path+"certificate.pem.crt")
+# Configure access to the AWS MQTT broker
+MQTTClient.configureEndpoint("apikey", 8883)
+MQTTClient.configureCredentials(
+    path + "root-CA.crt",
+    path + "private.pem.key",
+    path + "certificate.pem.crt"
+)
 
-# configure the MQTT broker
+# Configure MQTT client settings
 MQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 MQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 MQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 MQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-# register the callback
+# Register the callback for MQTT-SN client
 MQTTSNClient.registerCallback(Callback())
 
-# make connections to the clients
+# Establish connections to the MQTT and MQTT-SN brokers
 MQTTClient.connect()
 MQTTSNClient.connect()
 
-# let's ask the user which stations he wants to subscribe to
-# RiotOS MQTTSN clients publish to the topic sensor/station + id
-# the user will define only the id
+# Ask the user for station IDs to subscribe to
 station_ids = ""
-print("Enter the ID of the station, one by one, that you want to subscribe to.")
-print("Type 'stop' to interrupt the process.\n")
+print("Enter the ID of the station to subscribe to. Type 'stop' to interrupt.\n")
 while True:
     current_id = input("")
     if current_id == 'stop':
@@ -58,16 +58,16 @@ while True:
     else:
         station_ids += current_id + " "
 
-# subscribe to the topics choosen by the user
-for id in station_ids:
+# Subscribe to the topics chosen by the user
+for id in station_ids.split():
     MQTTSNClient.subscribe("sensor/station" + id)
 print("Subscribed to stations with ID: " + station_ids)
 
-# cycle that wait for a command to close the program
+# Wait for a command to exit the program
 while True:
-    if input("Enter 'quit' to exit from the program.\n")=="quit":
+    if input("Enter 'quit' to exit from the program.\n") == "quit":
         break
 
-# disconnect from the clients
+# Disconnect from the MQTT and MQTT-SN clients
 MQTTSNClient.disconnect()
 MQTTClient.disconnect()
